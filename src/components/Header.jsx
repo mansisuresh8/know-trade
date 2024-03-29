365dbcb70bd9b061402b190bf89b822c065f9983
mansisuresh8 <mansijumde0@gmail.com>
2024-03-30 01:07:36 +0530

    component: header-img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <Link href={"/"}>
+        <div className="w-32">
+          <Image
+            src={"/images/header/header-logo.png"}
+            height={50}
+            width={200}
+            className="object-contain w-full"
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
