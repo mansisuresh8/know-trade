65ec592d337d525518fa01687e69565d522a7672
mansisuresh8 <mansijumde0@gmail.com>
2024-04-05 00:23:13 +0530

    header start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,31 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <Link href={"/"}>
-        <div className="w-32">
-          <Image
-            src={"/images/header/header-logo.png"}
-            height={50}
-            width={200}
-            className="object-contain w-full"
-          />
+    <section className="px-20 py-8">
+      <div className="flex justify-between items-center">
+        <Link href={"/"}>
+          <div className="w-32">
+            <Image
+              src={"/images/header/header-logo.png"}
+              height={50}
+              width={200}
+              className="object-contain w-full"
+            />
+          </div>
+        </Link>
+        <div className="hidden lg:flex justify-center items-center gap-6 text-xl">
+          <p>About</p>
+          <p>Courses</p>
+          <p>Contact Us</p>
         </div>
-      </Link>
-    </div>
+        <section className="hidden lg:block">
+        <div className="bg-[#FF9426] text-white rounded-full border border-orange-6 px-4 py-1 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+          <button className="font-bold">Order Now</button>
+        </div>
+      </section>
+      </div>
+   
+    </section>
   );
 };
 
