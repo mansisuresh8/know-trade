07e87c106ec0d1626e2c39008d617de7aa52ec87
mansisuresh8 <mansijumde0@gmail.com>
2024-04-12 00:51:24 +0530

    start about

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -25,18 +27,58 @@ const HeroSection = () => {
           </section>
         </div>
         <div className="py-20">
-          <img src="./images/home/hero-section-2.png" alt="" />
+          <Link href={"/"}>
+            <div className="w-full">
+              <Image
+                src={"/images/home/hero-section-2.png"}
+                height={500}
+                width={500}
+                className="object-contain w-full"
+              />
+            </div>
+          </Link>{" "}
         </div>
       </div>
       <section className="bg-[#FF8CF1] px-8 py-8 flex justify-between items-center">
-      <div className="grid grid-cols-3 grid-rows-2 ">
-        <p className=" text-5xl font-bold">5+</p>
-        <p className=" text-5xl font-bold">2000+</p>
-        <p className=" text-5xl font-bold">3000+</p>
-        <p>5+ Years of Trading Experience</p>
-        <p>Mentored 2000 Students Across the world</p>
-        <p>A Community of 3000+ Succesfull Traders</p>
-      </div>
+        <div className="grid grid-cols-3 grid-rows-2 ">
+          <p className=" text-5xl font-bold">5+</p>
+          <p className=" text-5xl font-bold">2000+</p>
+          <p className=" text-5xl font-bold">3000+</p>
+          <p>5+ Years of Trading Experience</p>
+          <p>Mentored 2000 Students Across the world</p>
+          <p>A Community of 3000+ Succesfull Traders</p>
+        </div>
+      </section>
+
+      <section>
+        <div className="flex justify-center items-center px-20 py-20 gap-4">
+          <Link href={"/"}>
+            <div className="w-full">
+              <Image
+                src={"/images/home/about-1.png"}
+                height={500}
+                width={200}
+                className="object-contain w-full"
+              />
+            </div>
+          </Link>
+          <div className="">
+            <p>ABOUT KNOWTRADE</p>
+            <p>
+              We At KNOWTRADE Aim To Help Traders Around The World To Reach
+              Their Trading Potential In Global Markets. Over The Past 3 Years,
+              We Have Mentored More Than +500 Students To Help Them Become
+              Profitable Traders Across The Markets.
+            </p>
+            <p>
+              We help you get the potential to generate financial returns on
+              both rising and falling prices across forex, indices, commodities,
+              shares, oil, metals, and cryptocurrencies. Whether you’re an
+              experienced trader or competely new to it, we’re here to help you
+              find freedom in the financial markets.
+            </p>
+          </div>
+        </div>
       </section>
     </section>
   );
