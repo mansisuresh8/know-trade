e7b95064237b86b3c8a5a4c52be72b9252acab6b
mansisuresh8 <mansijumde0@gmail.com>
2024-04-17 19:57:11 +0530

    mentorSection Upadte

--- src/components/home/CoursesSection.jsx
+++ src/components/home/CoursesSection.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const CoursesSection = () => {
   return (
-    <section className="flex justify-center items-center bg-gradient-to-r to-[#FF8CF1] from-[#992FFF] w-full">
-      <div className="container px-4 lg:px-20 ">
+    <section className="flex flex-col justify-center items-center bg-gradient-to-r to-[#FF8CF1] from-[#992FFF] w-full">
+      <div className="flex flex-col justify-center items-center container px-4 lg:px-20 ">
         <div className="flex justify-center lg:justify-between items-center py-20 gap-4 lg:gap-16">
           <div className="w-full lg:w-1/3">
             <p>Our popular Courses</p>
@@ -51,7 +51,80 @@ const CoursesSection = () => {
               </div>
             </div>
           </div>
+          <div className="w-full lg:w-2/3">
+            <div className="grid grid-cols-3 ">
+              <div className="bg-white text-black rounded-lg px-4 py-6 ">
+                <div className="w-full">
+                  <Image
+                    src={"/images/home/courses-section-1.png"}
+                    alt=""
+                    height={500}
+                    width={500}
+                    className="object-contain w-full"
+                  />
+                </div>
+                <div>
+                  <div>
+                    <p>Trading 101</p>
+                    <p>( Beginner )</p>
+                  </div>
+                  <div>
+                    <div>
+                      <img src="" alt="" />
+                      <p>(15)</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <p></p>
+                    </div>
+                    <div>
+                      <button></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full lg:w-2/3">
+            <div className="grid grid-cols-3 ">
+              <div className="bg-white text-black rounded-lg px-4 py-6 ">
+                <div className="w-full">
+                  <Image
+                    src={"/images/home/courses-section-1.png"}
+                    alt=""
+                    height={500}
+                    width={500}
+                    className="object-contain w-full"
+                  />
+                </div>
+                <div>
+                  <div>
+                    <p>Trading 101</p>
+                    <p>( Beginner )</p>
+                  </div>
+                  <div>
+                    <div>
+                      <img src="" alt="" />
+                      <p>(15)</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <p></p>
+                    </div>
+                    <div>
+                      <button></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
         </div>
+    
+        
       </div>
     </section>
   );
--- src/components/home/MentorsSection.jsx
+++ src/components/home/MentorsSection.jsx
@@ -7,9 +7,9 @@ const MentorsSection = () => {
       <div className="container px-4 lg:px-20 ">
         <div className="flex flex-col justify-center items-center gap-4">
           <div className="flex justify-center items-center py-20">
-            <div>
-              <p>Mentors</p>
-              <p>
+            <div >
+              <p className="text-5xl font-semibold text-center text-[#992FFF] py-4">Mentors</p>
+              <p className="text-lg">
                 Meet the mentors who will guide you throughout your entire
                 journey.
               </p>
@@ -50,21 +50,33 @@ const MentorsSection = () => {
               </div>
             </div>
 
-            <div className="flex justify-between items-center">
-              <div>
-                <p>Been trading in the Indian and Forex Market for 3+years. </p>
-                <p>Trained & Mentored more than 500+ students. </p>
-                <p>NSE certified Options & Equity Derivatives Trader. </p>
-                <p>Successfully Handling portfolio worth $250000. </p>
-                <p>
-                  Focussed on creating a diverse educational platform enabling
-                  traders from all
-                </p>
-                <p>
-                  Backgrounds to reach their potential in the financial markets.
-                </p>
+            
+          </section>
+
+          <section className="flex flex-col justify-center items-center w-full gap-8">
+            <div className="flex justify-between items-center w-full gap-16">
+             
+
+              <div className="w-2/3">
+                <ul className="text-white list-disc text-2xl space-y-4">
+                  <li>
+                    Been trading in the Indian and Forex Market for 3+years.
+                  </li>
+                  <li>Trained & Mentored more than 500+ students. </li>
+                  <li>NSE certified Options & Equity Derivatives Trader. </li>
+                  <li>Successfully Handling portfolio worth $250000. </li>
+                  <li>
+                    Focussed on creating a diverse educational platform enabling
+                    traders from all
+                  </li>
+                  <li>
+                    Backgrounds to reach their potential in the financial
+                    markets.
+                  </li>
+                </ul>
               </div>
-              <div className=" ">
+
+              <div className="w-1/3">
                 <div className="w-full">
                   <Image
                     src={"/images/home/mentors-2.png"}
@@ -76,6 +88,8 @@ const MentorsSection = () => {
                 </div>
               </div>
             </div>
+
+            
           </section>
         </div>
       </div>
